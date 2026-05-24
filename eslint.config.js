0453fe388bb409f9e9da731c1e819571ba740ddf
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2026-05-24 10:05:38 +0200

    updates eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,7 @@ import neostandard, { plugins, resolveIgnoresFromGitignore } from 'neostandard'
 export default [
   {
     ignores: [
-      '**/node_modules/**',
-      'node_modules/**',
+      'node_modules/**'
     ]
   },
   ...neostandard({
